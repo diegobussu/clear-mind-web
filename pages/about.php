c9724b376255b3e3885008ab26ddd4ae5c3c215a
DontitoSama <jokonito06@gmail.com>
2024-05-30 21:15:35 +0200

    :pencil: Add about.php

---
+++ pages/about.php
@@ -0,0 +1,57 @@
+<?php
+// Inclure le fichier de configuration
+include __DIR__ . '../../config/config.php';
+
+// Utiliser la constante BASE_URL définie dans config.php
+$base_url = get_base_url();
+?>
+
+<!-- Head Section -->
+<?php include __DIR__ . '../../components/head.php'; ?>
+
+<title>Clear Mind - Nous découvrir</title>
+
+<body class="body-2">
+    <!-- Header Section -->
+    <?php include __DIR__ . '../../components/header.php'; ?>
+
+    <main class="margin-large text-align-center">
+        <div class="section">
+            <h3 class="heading-style-h3">Nous découvrir</h3>
+        </div>
+    </main>
+
+    <section class="section_cta">
+        <div class="padding-global">
+            <div class="container-small">
+                <div class="line-decal hide-mobile-landscape"></div>
+                <div class="padding-section-medium">
+                    <div class="text-align-center">
+                        <div class="margin-bottom margin-large">
+                            <h3 class="heading-style-h3">Commencez votre journal avec Clear Mind aujourd&#39;hui.</h3>
+                        </div>
+                        <p class="text-size-xxlarge">Obtenez gratuitement un journal accessible et sécurisé, une aide et un suivi personnalisé !</p>
+                        <div class="margin-top margin-medium">
+                            <div class="button-group is-center">
+                                <a href="#" target="_blank" class="link-style_app w-inline-block app-link">
+                                    <img src="<?php echo get_base_url(); ?>public/img/apple-store.svg" width="167" alt="Download on the App Store button."/>
+                                </a>
+                                <a href="#" target="_blank" class="link-style_app w-inline-block app-link">
+                                    <img src="<?php echo get_base_url(); ?>public/img/google-play.svg" width="175" alt="Download on the Google Play button."/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="line-decal hide-mobile-landscape"></div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer Section -->
+    <?php include __DIR__ . '../../components/footer.php'; ?>
+
+    <script src="<?php echo get_base_url(); ?>public/js/jquery-3.5.1.min.js" type="text/javascript"></script>
+    <script src="<?php echo get_base_url(); ?>public/js/index.js" type="text/javascript"></script>
+</body>
+</html>
