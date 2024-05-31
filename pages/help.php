bc872db418f20cfce8c626b4ee9c13fd8d5354f0
DontitoSama <jokonito06@gmail.com>
2024-05-31 14:27:59 +0200

    :pencil: replace img for help section

--- pages/help.php
+++ pages/help.php
@@ -21,7 +21,7 @@ $base_url = get_base_url();
         <div class="help-column">
             <a href="articles/started" class="help-link">
                 <div class="help-block">
-                    <img src="<?php echo get_base_url(); ?>public/img/started.svg" class="help-image" alt="Started icon">
+                    <img src="https://firebasestorage.googleapis.com/v0/b/my-digital-project-aa48f.appspot.com/o/web%2Fstarted.svg?alt=media&token=6ea07927-4c08-41db-84a9-b55dfc45a1ae" class="help-image" alt="Started icon">
                     <h5 class="heading-style-h5">Premiers pas</h5>
                     <p class="text-size-large text-color-grey">Découvrez les étapes essentielles pour commencer à utiliser notre application.</p>
                     <div class="text-style-pre-header">4 articles</div>
@@ -30,7 +30,7 @@ $base_url = get_base_url();
 
             <a href="articles/features" class="help-link">
                 <div class="help-block">
-                    <img src="<?php echo get_base_url(); ?>public/img/features.svg" class="help-image" alt="Features icon">
+                    <img src="https://firebasestorage.googleapis.com/v0/b/my-digital-project-aa48f.appspot.com/o/web%2Ffeatures.svg?alt=media&token=29763224-f751-4fbb-aa1a-855e165c9b9d" class="help-image" alt="Features icon">
                     <h5 class="heading-style-h5">Personnalisation</h5>
                     <p class="text-size-large text-color-grey">Explorez diverses options de personnalisation pour adapter l'application à vos besoins.</p>
                     <div class="text-style-pre-header">5 articles</div>
@@ -39,7 +39,7 @@ $base_url = get_base_url();
 
             <a href="articles/privacy" class="help-link">
                 <div class="help-block">
-                    <img src="<?php echo get_base_url(); ?>public/img/privacy.svg" class="help-image" alt="Privacy icon">
+                    <img src="https://firebasestorage.googleapis.com/v0/b/my-digital-project-aa48f.appspot.com/o/web%2Fprivacy.svg?alt=media&token=e35349f2-d7a7-4de3-9c38-f4a0c21c8620" class="help-image" alt="Privacy icon">
                     <h5 class="heading-style-h5">Confidentialité et sécurité</h5>
                     <p class="text-size-large text-color-grey">Informez-vous sur nos mesures de sécurité et comment nous protégeons vos données.</p>
                     <div class="text-style-pre-header">3 articles</div>
@@ -51,7 +51,7 @@ $base_url = get_base_url();
 
             <a href="articles/widgets" class="help-link">
                 <div class="help-block">
-                    <img src="<?php echo get_base_url(); ?>public/img/widgets.svg" class="help-image" alt="Widgets icon">
+                    <img src="https://firebasestorage.googleapis.com/v0/b/my-digital-project-aa48f.appspot.com/o/web%2Fwidgets.svg?alt=media&token=c926c625-239b-4037-abb0-1ffd62d671d1" class="help-image" alt="Widgets icon">
                     <h5 class="heading-style-h5">Widgets</h5>
                     <p class="text-size-large text-color-grey">Apprenez à personnaliser et utiliser les widgets pour améliorer votre expérience utilisateur.</p>
                     <div class="text-style-pre-header">4 articles</div>
@@ -60,7 +60,7 @@ $base_url = get_base_url();
 
             <a href="articles/premium" class="help-link">
                 <div class="help-block">
-                    <img src="<?php echo get_base_url(); ?>public/img/premium.svg" class="help-image" alt="Premium icon">
+                    <img src="https://firebasestorage.googleapis.com/v0/b/my-digital-project-aa48f.appspot.com/o/web%2Fpremium.svg?alt=media&token=a274d782-16ba-464c-9d40-123770cddd3a" class="help-image" alt="Premium icon">
                     <h5 class="heading-style-h5">Clear Mind Premium</h5>
                     <p class="text-size-large text-color-grey">Découvrez les fonctionnalités exclusives de notre abonnement premium.</p>
                     <div class="text-style-pre-header">2 articles</div>
@@ -69,7 +69,7 @@ $base_url = get_base_url();
 
             <a href="articles/other" class="help-link">
                 <div class="help-block">
-                    <img src="<?php echo get_base_url(); ?>public/img/other.svg" class="help-image" alt="Other icon">
+                    <img src="https://firebasestorage.googleapis.com/v0/b/my-digital-project-aa48f.appspot.com/o/web%2Fother.svg?alt=media&token=7e408839-e140-492f-9cf2-f840e66a95e2" class="help-image" alt="Other icon">
                     <h5 class="heading-style-h5">Divers</h5>
                     <p class="text-size-large text-color-grey">Retrouvez une variété d'articles sur des sujets divers liés à l'application.</p>
                     <div class="text-style-pre-header">7 articles</div>
