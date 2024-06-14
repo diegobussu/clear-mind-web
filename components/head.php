893dc458fb5f02412aea0ee30f4ce98bf4efb6f5
DontitoSama <jokonito06@gmail.com>
2024-06-14 18:07:12 +0200

    :pencil: add meta

--- components/head.php
+++ components/head.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Clear Mind - Tenir un journal n'a jamais été aussi facile. Journal sécurisé avec un suivi d'humeur et une aide personnalisée à votre santé mentale.">
+    <meta name="keywords" content="journal, santé mentale, suivi d'humeur, journal intime, guides personnalisés, thérapie">
+    <meta name="author" content="Clear Mind Team">
     <link rel="shortcut icon" href="https://firebasestorage.googleapis.com/v0/b/my-digital-project-aa48f.appspot.com/o/web%2Ffavicon.svg?alt=media&token=cc84cc8e-36e2-45cc-8eaa-ad659a7560aa" type="image/x-icon">
     <link href="<?php echo get_base_url(); ?>public/css/index.css" rel="stylesheet" type="text/css"/>
+    <title>Clear Mind</title>
 </head>
